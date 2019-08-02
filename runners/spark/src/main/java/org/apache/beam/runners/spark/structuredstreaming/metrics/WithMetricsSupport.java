apache/beam

Added metrics sinks and tests

runners/spark/src/main/java/org/apache/beam/runners/spark/structuredstreaming/metrics/WithMetricsSupport.java
@@ -1,0 +1,179 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.metrics;
+
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.Metric;
+import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+import org.apache.beam.runners.spark.structuredstreaming.aggregators.NamedAggregators;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.base.Function;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.base.Optional;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.base.Predicate;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.base.Predicates;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.collect.FluentIterable;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.collect.ImmutableSortedMap;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.collect.Maps;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.collect.Ordering;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedMap;
+
+/**
+ * A {@link MetricRegistry} decorator-like that supports {@link AggregatorMetric} and {@link
+ * SparkBeamMetric} as {@link Gauge Gauges}.
+ *
+ * <p>{@link MetricRegistry} is not an interface, so this is not a by-the-book decorator. That said,
+ * it delegates all metric related getters to the "decorated" instance.
+ */
+public class WithMetricsSupport extends MetricRegistry {
+
+  private static final Logger LOG = LoggerFactory.getLogger(
+      WithMetricsSupport.class);
+
+  private final MetricRegistry internalMetricRegistry;
+
+  private WithMetricsSupport(final MetricRegistry internalMetricRegistry) {
+    this.internalMetricRegistry = internalMetricRegistry;
+  }
+
+  public static WithMetricsSupport forRegistry(final MetricRegistry metricRegistry) {
+    return new WithMetricsSupport(metricRegistry);
+  }
+
+  @Override
+  public SortedMap<String, Timer> getTimers(final MetricFilter filter) {
+    return internalMetricRegistry.getTimers(filter);
+  }
+
+  @Override
+  public SortedMap<String, Meter> getMeters(final MetricFilter filter) {
+    return internalMetricRegistry.getMeters(filter);
+  }
+
+  @Override
+  public SortedMap<String, Histogram> getHistograms(final MetricFilter filter) {
+    return internalMetricRegistry.getHistograms(filter);
+  }
+
+  @Override
+  public SortedMap<String, Counter> getCounters(final MetricFilter filter) {
+    return internalMetricRegistry.getCounters(filter);
+  }
+
+  @Override
+  public SortedMap<String, Gauge> getGauges(final MetricFilter filter) {
+    return new ImmutableSortedMap.Builder<String, Gauge>(
+            Ordering.from(String.CASE_INSENSITIVE_ORDER))
+        .putAll(internalMetricRegistry.getGauges(filter))
+        .putAll(extractGauges(internalMetricRegistry, filter))
+        .build();
+  }
+
+  private Map<String, Gauge> extractGauges(
+      final MetricRegistry metricRegistry, final MetricFilter filter) {
+    Map<String, Gauge> gauges = new HashMap<>();
+
+    // find the AggregatorMetric metrics from within all currently registered metrics
+    final Optional<Map<String, Gauge>> aggregatorMetrics =
+        FluentIterable.from(metricRegistry.getMetrics().entrySet())
+            .firstMatch(isAggregatorMetric())
+            .transform(aggregatorMetricToGauges());
+
+    // find the SparkBeamMetric metrics from within all currently registered metrics
+    final Optional<Map<String, Gauge>> beamMetrics =
+        FluentIterable.from(metricRegistry.getMetrics().entrySet())
+            .firstMatch(isSparkBeamMetric())
+            .transform(beamMetricToGauges());
+
+    if (aggregatorMetrics.isPresent()) {
+      gauges.putAll(Maps.filterEntries(aggregatorMetrics.get(), matches(filter)));
+    }
+
+    if (beamMetrics.isPresent()) {
+      gauges.putAll(Maps.filterEntries(beamMetrics.get(), matches(filter)));
+    }
+
+    return gauges;
+  }
+
+  private Function<Map.Entry<String, Metric>, Map<String, Gauge>> aggregatorMetricToGauges() {
+    return entry -> {
+      final NamedAggregators agg = ((AggregatorMetric) entry.getValue()).getNamedAggregators();
+      final String parentName = entry.getKey();
+      final Map<String, Gauge> gaugeMap = Maps.transformEntries(agg.renderAll(), toGauge());
+      final Map<String, Gauge> fullNameGaugeMap = Maps.newLinkedHashMap();
+      for (Map.Entry<String, Gauge> gaugeEntry : gaugeMap.entrySet()) {
+        fullNameGaugeMap.put(parentName + "." + gaugeEntry.getKey(), gaugeEntry.getValue());
+      }
+      return Maps.filterValues(fullNameGaugeMap, Predicates.notNull());
+    };
+  }
+
+  private Function<Map.Entry<String, Metric>, Map<String, Gauge>> beamMetricToGauges() {
+    return entry -> {
+      final Map<String, ?> metrics = ((SparkBeamMetric) entry.getValue()).renderAll();
+      final String parentName = entry.getKey();
+      final Map<String, Gauge> gaugeMap = Maps.transformEntries(metrics, toGauge());
+      final Map<String, Gauge> fullNameGaugeMap = Maps.newLinkedHashMap();
+      for (Map.Entry<String, Gauge> gaugeEntry : gaugeMap.entrySet()) {
+        fullNameGaugeMap.put(parentName + "." + gaugeEntry.getKey(), gaugeEntry.getValue());
+      }
+      return Maps.filterValues(fullNameGaugeMap, Predicates.notNull());
+    };
+  }
+
+  private Maps.EntryTransformer<String, Object, Gauge> toGauge() {
+    return (name, rawValue) ->
+        () -> {
+          // at the moment the metric's type is assumed to be
+          // compatible with Double. While far from perfect, it seems reasonable at
+          // this point in time
+          try {
+            return Double.parseDouble(rawValue.toString());
+          } catch (final Exception e) {
+            LOG.warn(
+                "Failed reporting metric with name [{}], of type [{}], since it could not be"
+                    + " converted to double",
+                name,
+                rawValue.getClass().getSimpleName(),
+                e);
+            return null;
+          }
+        };
+  }
+
+  private Predicate<Map.Entry<String, Gauge>> matches(final MetricFilter filter) {
+    return entry -> filter.matches(entry.getKey(), entry.getValue());
+  }
+
+  private Predicate<Map.Entry<String, Metric>> isAggregatorMetric() {
+    return metricEntry -> (metricEntry.getValue() instanceof AggregatorMetric);
+  }
+
+  private Predicate<Map.Entry<String, Metric>> isSparkBeamMetric() {
+    return metricEntry -> (metricEntry.getValue() instanceof SparkBeamMetric);
+  }
+}

runners/spark/src/test/java/org/apache/beam/runners/spark/structuredstreaming/aggregators/metrics/sink/InMemoryMetrics.java
@@ -1,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.aggregators.metrics.sink;
+
+import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.MetricRegistry;
+import org.apache.beam.runners.spark.structuredstreaming.metrics.WithMetricsSupport;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.base.Predicates;
+import org.apache.spark.metrics.sink.Sink;
+
+import java.util.Properties;
+
+/** An in-memory {@link Sink} implementation for tests. */
+public class InMemoryMetrics implements Sink {
+
+  private static WithMetricsSupport extendedMetricsRegistry;
+  private static MetricRegistry internalMetricRegistry;
+
+  @SuppressWarnings("UnusedParameters")
+  public InMemoryMetrics(
+      final Properties properties,
+      final MetricRegistry metricRegistry,
+      final org.apache.spark.SecurityManager securityMgr) {
+    extendedMetricsRegistry = WithMetricsSupport.forRegistry(metricRegistry);
+    internalMetricRegistry = metricRegistry;
+  }
+
+  @SuppressWarnings("TypeParameterUnusedInFormals")
+  public static <T> T valueOf(final String name) {
+    final T retVal;
+
+    // this might fail in case we have multiple aggregators with the same suffix after
+    // the last dot, but it should be good enough for tests.
+    if (extendedMetricsRegistry != null
+        && extendedMetricsRegistry.getGauges().keySet().stream()
+            .anyMatch(Predicates.containsPattern(name + "$")::apply)) {
+      String key =
+          extendedMetricsRegistry.getGauges().keySet().stream()
+              .filter(Predicates.containsPattern(name + "$")::apply)
+              .findFirst()
+              .get();
+      retVal = (T) extendedMetricsRegistry.getGauges().get(key).getValue();
+    } else {
+      retVal = null;
+    }
+
+    return retVal;
+  }
+
+  @SuppressWarnings("WeakerAccess")
+  public static void clearAll() {
+    if (internalMetricRegistry != null) {
+      internalMetricRegistry.removeMatching(MetricFilter.ALL);
+    }
+  }
+
+  @Override
+  public void start() {}
+
+  @Override
+  public void stop() {}
+
+  @Override
+  public void report() {}
+}

runners/spark/src/test/java/org/apache/beam/runners/spark/structuredstreaming/aggregators/metrics/sink/SparkMetricsSinkTest.java
@@ -1,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.aggregators.metrics.sink;
+
+import org.apache.beam.runners.spark.structuredstreaming.examples.WordCount;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.collect.ImmutableList;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.collect.ImmutableSet;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExternalResource;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * TODO: add testInStreamingMode() once streaming support will be implemented.
+ *
+ * A test that verifies Beam metrics are reported to Spark's metrics sink in both batch and
+ * streaming modes.
+ */
+public class SparkMetricsSinkTest {
+  @Rule public ExternalResource inMemoryMetricsSink = new InMemoryMetricsSinkRule();
+  @Rule public final TestPipeline pipeline = TestPipeline.create();
+
+  private static final ImmutableList<String> WORDS =
+      ImmutableList.of("hi there", "hi", "hi sue bob", "hi sue", "", "bob hi");
+  private static final ImmutableSet<String> EXPECTED_COUNTS =
+      ImmutableSet.of("hi: 5", "there: 1", "sue: 2", "bob: 2");
+
+  @Test
+  public void testInBatchMode() throws Exception {
+    assertThat(InMemoryMetrics.valueOf("emptyLines"), is(nullValue()));
+
+    final PCollection<String> output =
+        pipeline
+            .apply(Create.of(WORDS).withCoder(StringUtf8Coder.of()))
+            .apply(new WordCount.CountWords())
+            .apply(MapElements.via(new WordCount.FormatAsTextFn()));
+    PAssert.that(output).containsInAnyOrder(EXPECTED_COUNTS);
+    pipeline.run();
+
+    assertThat(InMemoryMetrics.<Double>valueOf("emptyLines"), is(1d));
+  }
+}

samuelmoss572/dataflowworkshop

runners/spark/src/main/java/org/apache/beam/runners/spark/structuredstreaming/metrics/WithMetricsSupport.java
@@ -1,0 +1,179 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.metrics;
+
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.Metric;
+import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+import org.apache.beam.runners.spark.structuredstreaming.aggregators.NamedAggregators;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Function;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Optional;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Predicate;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Predicates;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.FluentIterable;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.ImmutableSortedMap;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.Maps;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.Ordering;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedMap;
+
+/**
+ * A {@link MetricRegistry} decorator-like that supports {@link AggregatorMetric} and {@link
+ * SparkBeamMetric} as {@link Gauge Gauges}.
+ *
+ * <p>{@link MetricRegistry} is not an interface, so this is not a by-the-book decorator. That said,
+ * it delegates all metric related getters to the "decorated" instance.
+ */
+public class WithMetricsSupport extends MetricRegistry {
+
+  private static final Logger LOG = LoggerFactory.getLogger(
+      WithMetricsSupport.class);
+
+  private final MetricRegistry internalMetricRegistry;
+
+  private WithMetricsSupport(final MetricRegistry internalMetricRegistry) {
+    this.internalMetricRegistry = internalMetricRegistry;
+  }
+
+  public static WithMetricsSupport forRegistry(final MetricRegistry metricRegistry) {
+    return new WithMetricsSupport(metricRegistry);
+  }
+
+  @Override
+  public SortedMap<String, Timer> getTimers(final MetricFilter filter) {
+    return internalMetricRegistry.getTimers(filter);
+  }
+
+  @Override
+  public SortedMap<String, Meter> getMeters(final MetricFilter filter) {
+    return internalMetricRegistry.getMeters(filter);
+  }
+
+  @Override
+  public SortedMap<String, Histogram> getHistograms(final MetricFilter filter) {
+    return internalMetricRegistry.getHistograms(filter);
+  }
+
+  @Override
+  public SortedMap<String, Counter> getCounters(final MetricFilter filter) {
+    return internalMetricRegistry.getCounters(filter);
+  }
+
+  @Override
+  public SortedMap<String, Gauge> getGauges(final MetricFilter filter) {
+    return new ImmutableSortedMap.Builder<String, Gauge>(
+            Ordering.from(String.CASE_INSENSITIVE_ORDER))
+        .putAll(internalMetricRegistry.getGauges(filter))
+        .putAll(extractGauges(internalMetricRegistry, filter))
+        .build();
+  }
+
+  private Map<String, Gauge> extractGauges(
+      final MetricRegistry metricRegistry, final MetricFilter filter) {
+    Map<String, Gauge> gauges = new HashMap<>();
+
+    // find the AggregatorMetric metrics from within all currently registered metrics
+    final Optional<Map<String, Gauge>> aggregatorMetrics =
+        FluentIterable.from(metricRegistry.getMetrics().entrySet())
+            .firstMatch(isAggregatorMetric())
+            .transform(aggregatorMetricToGauges());
+
+    // find the SparkBeamMetric metrics from within all currently registered metrics
+    final Optional<Map<String, Gauge>> beamMetrics =
+        FluentIterable.from(metricRegistry.getMetrics().entrySet())
+            .firstMatch(isSparkBeamMetric())
+            .transform(beamMetricToGauges());
+
+    if (aggregatorMetrics.isPresent()) {
+      gauges.putAll(Maps.filterEntries(aggregatorMetrics.get(), matches(filter)));
+    }
+
+    if (beamMetrics.isPresent()) {
+      gauges.putAll(Maps.filterEntries(beamMetrics.get(), matches(filter)));
+    }
+
+    return gauges;
+  }
+
+  private Function<Map.Entry<String, Metric>, Map<String, Gauge>> aggregatorMetricToGauges() {
+    return entry -> {
+      final NamedAggregators agg = ((AggregatorMetric) entry.getValue()).getNamedAggregators();
+      final String parentName = entry.getKey();
+      final Map<String, Gauge> gaugeMap = Maps.transformEntries(agg.renderAll(), toGauge());
+      final Map<String, Gauge> fullNameGaugeMap = Maps.newLinkedHashMap();
+      for (Map.Entry<String, Gauge> gaugeEntry : gaugeMap.entrySet()) {
+        fullNameGaugeMap.put(parentName + "." + gaugeEntry.getKey(), gaugeEntry.getValue());
+      }
+      return Maps.filterValues(fullNameGaugeMap, Predicates.notNull());
+    };
+  }
+
+  private Function<Map.Entry<String, Metric>, Map<String, Gauge>> beamMetricToGauges() {
+    return entry -> {
+      final Map<String, ?> metrics = ((SparkBeamMetric) entry.getValue()).renderAll();
+      final String parentName = entry.getKey();
+      final Map<String, Gauge> gaugeMap = Maps.transformEntries(metrics, toGauge());
+      final Map<String, Gauge> fullNameGaugeMap = Maps.newLinkedHashMap();
+      for (Map.Entry<String, Gauge> gaugeEntry : gaugeMap.entrySet()) {
+        fullNameGaugeMap.put(parentName + "." + gaugeEntry.getKey(), gaugeEntry.getValue());
+      }
+      return Maps.filterValues(fullNameGaugeMap, Predicates.notNull());
+    };
+  }
+
+  private Maps.EntryTransformer<String, Object, Gauge> toGauge() {
+    return (name, rawValue) ->
+        () -> {
+          // at the moment the metric's type is assumed to be
+          // compatible with Double. While far from perfect, it seems reasonable at
+          // this point in time
+          try {
+            return Double.parseDouble(rawValue.toString());
+          } catch (final Exception e) {
+            LOG.warn(
+                "Failed reporting metric with name [{}], of type [{}], since it could not be"
+                    + " converted to double",
+                name,
+                rawValue.getClass().getSimpleName(),
+                e);
+            return null;
+          }
+        };
+  }
+
+  private Predicate<Map.Entry<String, Gauge>> matches(final MetricFilter filter) {
+    return entry -> filter.matches(entry.getKey(), entry.getValue());
+  }
+
+  private Predicate<Map.Entry<String, Metric>> isAggregatorMetric() {
+    return metricEntry -> (metricEntry.getValue() instanceof AggregatorMetric);
+  }
+
+  private Predicate<Map.Entry<String, Metric>> isSparkBeamMetric() {
+    return metricEntry -> (metricEntry.getValue() instanceof SparkBeamMetric);
+  }
+}

runners/spark/src/test/java/org/apache/beam/runners/spark/structuredstreaming/aggregators/metrics/sink/InMemoryMetrics.java
@@ -1,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.aggregators.metrics.sink;
+
+import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.MetricRegistry;
+import org.apache.beam.runners.spark.structuredstreaming.metrics.WithMetricsSupport;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Predicates;
+import org.apache.spark.metrics.sink.Sink;
+
+import java.util.Properties;
+
+/** An in-memory {@link Sink} implementation for tests. */
+public class InMemoryMetrics implements Sink {
+
+  private static WithMetricsSupport extendedMetricsRegistry;
+  private static MetricRegistry internalMetricRegistry;
+
+  @SuppressWarnings("UnusedParameters")
+  public InMemoryMetrics(
+      final Properties properties,
+      final MetricRegistry metricRegistry,
+      final org.apache.spark.SecurityManager securityMgr) {
+    extendedMetricsRegistry = WithMetricsSupport.forRegistry(metricRegistry);
+    internalMetricRegistry = metricRegistry;
+  }
+
+  @SuppressWarnings("TypeParameterUnusedInFormals")
+  public static <T> T valueOf(final String name) {
+    final T retVal;
+
+    // this might fail in case we have multiple aggregators with the same suffix after
+    // the last dot, but it should be good enough for tests.
+    if (extendedMetricsRegistry != null
+        && extendedMetricsRegistry.getGauges().keySet().stream()
+            .anyMatch(Predicates.containsPattern(name + "$")::apply)) {
+      String key =
+          extendedMetricsRegistry.getGauges().keySet().stream()
+              .filter(Predicates.containsPattern(name + "$")::apply)
+              .findFirst()
+              .get();
+      retVal = (T) extendedMetricsRegistry.getGauges().get(key).getValue();
+    } else {
+      retVal = null;
+    }
+
+    return retVal;
+  }
+
+  @SuppressWarnings("WeakerAccess")
+  public static void clearAll() {
+    if (internalMetricRegistry != null) {
+      internalMetricRegistry.removeMatching(MetricFilter.ALL);
+    }
+  }
+
+  @Override
+  public void start() {}
+
+  @Override
+  public void stop() {}
+
+  @Override
+  public void report() {}
+}

runners/spark/src/test/java/org/apache/beam/runners/spark/structuredstreaming/aggregators/metrics/sink/SparkMetricsSinkTest.java
@@ -1,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.aggregators.metrics.sink;
+
+import org.apache.beam.runners.spark.structuredstreaming.examples.WordCount;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.ImmutableList;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.ImmutableSet;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExternalResource;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * TODO: add testInStreamingMode() once streaming support will be implemented.
+ *
+ * A test that verifies Beam metrics are reported to Spark's metrics sink in both batch and
+ * streaming modes.
+ */
+public class SparkMetricsSinkTest {
+  @Rule public ExternalResource inMemoryMetricsSink = new InMemoryMetricsSinkRule();
+  @Rule public final TestPipeline pipeline = TestPipeline.create();
+
+  private static final ImmutableList<String> WORDS =
+      ImmutableList.of("hi there", "hi", "hi sue bob", "hi sue", "", "bob hi");
+  private static final ImmutableSet<String> EXPECTED_COUNTS =
+      ImmutableSet.of("hi: 5", "there: 1", "sue: 2", "bob: 2");
+
+  @Test
+  public void testInBatchMode() throws Exception {
+    assertThat(InMemoryMetrics.valueOf("emptyLines"), is(nullValue()));
+
+    final PCollection<String> output =
+        pipeline
+            .apply(Create.of(WORDS).withCoder(StringUtf8Coder.of()))
+            .apply(new WordCount.CountWords())
+            .apply(MapElements.via(new WordCount.FormatAsTextFn()));
+    PAssert.that(output).containsInAnyOrder(EXPECTED_COUNTS);
+    pipeline.run();
+
+    assertThat(InMemoryMetrics.<Double>valueOf("emptyLines"), is(1d));
+  }
+}