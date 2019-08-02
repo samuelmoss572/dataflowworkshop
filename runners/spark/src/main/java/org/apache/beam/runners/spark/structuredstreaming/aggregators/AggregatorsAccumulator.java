ve-global/beam

Add metrics support in DoFn

runners/spark/src/main/java/org/apache/beam/runners/spark/structuredstreaming/aggregators/AggregatorsAccumulator.java
@@ -1,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.aggregators;
+
+import org.apache.beam.vendor.guava.v20_0.com.google.common.annotations.VisibleForTesting;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.util.AccumulatorV2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * For resilience, {@link AccumulatorV2 Accumulators} are required to be wrapped in a Singleton.
+ *
+ * @see <a
+ *     href="https://spark.apache.org/docs/latest/api/java/org/apache/spark/util/AccumulatorV2.html">accumulatorsV2</a>
+ */
+public class AggregatorsAccumulator {
+  private static final Logger LOG = LoggerFactory.getLogger(AggregatorsAccumulator.class);
+
+  private static final String ACCUMULATOR_NAME = "Beam.Aggregators";
+
+  private static volatile NamedAggregatorsAccumulator instance = null;
+
+  /** Init aggregators accumulator if it has not been initiated. This method is idempotent. */
+  public static void init(JavaSparkContext jsc) {
+    if (instance == null) {
+      synchronized (AggregatorsAccumulator.class) {
+        if (instance == null) {
+          NamedAggregators namedAggregators = new NamedAggregators();
+          NamedAggregatorsAccumulator accumulator =
+              new NamedAggregatorsAccumulator(namedAggregators);
+          jsc.sc().register(accumulator, ACCUMULATOR_NAME);
+
+          instance = accumulator;
+        }
+      }
+      LOG.info("Instantiated aggregators accumulator: " + instance.value());
+    }
+  }
+
+  public static NamedAggregatorsAccumulator getInstance() {
+    if (instance == null) {
+      throw new IllegalStateException("Aggregrators accumulator has not been instantiated");
+    } else {
+      return instance;
+    }
+  }
+
+  @VisibleForTesting
+  public static void clear() {
+    synchronized (AggregatorsAccumulator.class) {
+      instance = null;
+    }
+  }
+}

runners/spark/src/main/java/org/apache/beam/runners/spark/structuredstreaming/aggregators/NamedAggregators.java
@@ -1,0 +1,112 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.aggregators;
+
+import org.apache.beam.sdk.transforms.Combine;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.collect.ImmutableMap;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.collect.Maps;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * This class wraps a map of named aggregators. Spark expects that all accumulators be declared
+ * before a job is launched. Beam allows aggregators to be used and incremented on the fly. We
+ * create a map of named aggregators and instantiate in the the spark context before the job is
+ * launched. We can then add aggregators on the fly in Spark.
+ */
+public class NamedAggregators implements Serializable {
+  /** Map from aggregator name to current state. */
+  private final Map<String, State<?, ?, ?>> mNamedAggregators = new TreeMap<>();
+
+  /** Constructs a new NamedAggregators instance. */
+  public NamedAggregators() {}
+
+  /**
+   * @param name Name of aggregator to retrieve.
+   * @param typeClass Type class to cast the value to.
+   * @param <T> Type to be returned.
+   * @return the value of the aggregator associated with the specified name, or <code>null</code> if
+   *     the specified aggregator could not be found.
+   */
+  public <T> T getValue(String name, Class<T> typeClass) {
+    final State<?, ?, ?> state = mNamedAggregators.get(name);
+    return state != null ? typeClass.cast(state.render()) : null;
+  }
+
+  /** @return a map of all the aggregator names and their <b>rendered </b>values */
+  public Map<String, ?> renderAll() {
+    return ImmutableMap.copyOf(Maps.transformValues(mNamedAggregators, State::render));
+  }
+
+  /**
+   * Merges another NamedAggregators instance with this instance.
+   *
+   * @param other The other instance of named aggregators ot merge.
+   * @return This instance of Named aggregators with associated states updated to reflect the other
+   *     instance's aggregators.
+   */
+  public NamedAggregators merge(
+      NamedAggregators other) {
+    for (Map.Entry<String, State<?, ?, ?>> e : other.mNamedAggregators.entrySet()) {
+      String key = e.getKey();
+      State<?, ?, ?> otherValue = e.getValue();
+      mNamedAggregators.merge(key, otherValue, NamedAggregators::merge);
+    }
+    return this;
+  }
+
+  /**
+   * Helper method to merge States whose generic types aren't provably the same, so require some
+   * casting.
+   */
+  @SuppressWarnings("unchecked")
+  private static <InputT, InterT, OutputT> State<InputT, InterT, OutputT> merge(
+      State<?, ?, ?> s1, State<?, ?, ?> s2) {
+    return ((State<InputT, InterT, OutputT>) s1).merge((State<InputT, InterT, OutputT>) s2);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    for (Map.Entry<String, State<?, ?, ?>> e : mNamedAggregators.entrySet()) {
+      sb.append(e.getKey()).append(": ").append(e.getValue().render()).append(" ");
+    }
+    return sb.toString();
+  }
+
+  /**
+   * @param <InputT> Input data type
+   * @param <InterT> Intermediate data type (useful for averages)
+   * @param <OutputT> Output data type
+   */
+  public interface State<InputT, InterT, OutputT> extends Serializable {
+
+    /** @param element new element to update state */
+    void update(InputT element);
+
+    State<InputT, InterT, OutputT> merge(State<InputT, InterT, OutputT> other);
+
+    InterT current();
+
+    OutputT render();
+
+    Combine.CombineFn<InputT, InterT, OutputT> getCombineFn();
+  }
+}

runners/spark/src/main/java/org/apache/beam/runners/spark/structuredstreaming/metrics/MetricsAccumulator.java
@@ -1,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.metrics;
+
+import org.apache.beam.runners.core.metrics.MetricsContainerStepMap;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.annotations.VisibleForTesting;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.util.AccumulatorV2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * For resilience, {@link AccumulatorV2 Accumulators} are required to be wrapped in a Singleton.
+ *
+ * @see <a
+ *     href="https://spark.apache.org/docs/latest/api/java/org/apache/spark/util/AccumulatorV2.html">accumulatorsV2</a>
+ */
+public class MetricsAccumulator {
+  private static final Logger LOG = LoggerFactory.getLogger(MetricsAccumulator.class);
+
+  private static final String ACCUMULATOR_NAME = "Beam.Metrics";
+
+  private static volatile MetricsContainerStepMapAccumulator instance = null;
+
+  /** Init metrics accumulator if it has not been initiated. This method is idempotent. */
+  public static void init(JavaSparkContext jsc) {
+    if (instance == null) {
+      synchronized (MetricsAccumulator.class) {
+        if (instance == null) {
+          MetricsContainerStepMap metricsContainerStepMap = new MetricsContainerStepMap();
+          MetricsContainerStepMapAccumulator accumulator =
+              new MetricsContainerStepMapAccumulator(metricsContainerStepMap);
+          jsc.sc().register(accumulator, ACCUMULATOR_NAME);
+
+          instance = accumulator;
+        }
+      }
+      LOG.info("Instantiated metrics accumulator: " + instance.value());
+    }
+  }
+
+  public static MetricsContainerStepMapAccumulator getInstance() {
+    if (instance == null) {
+      throw new IllegalStateException("Metrics accumulator has not been instantiated");
+    } else {
+      return instance;
+    }
+  }
+
+  @VisibleForTesting
+  public static void clear() {
+    synchronized (MetricsAccumulator.class) {
+      instance = null;
+    }
+  }
+}

runners/spark/src/main/java/org/apache/beam/runners/spark/structuredstreaming/metrics/SparkBeamMetric.java
@@ -1,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.metrics;
+
+import com.codahale.metrics.Metric;
+import org.apache.beam.runners.core.metrics.MetricsContainerStepMap;
+import org.apache.beam.sdk.metrics.DistributionResult;
+import org.apache.beam.sdk.metrics.GaugeResult;
+import org.apache.beam.sdk.metrics.MetricKey;
+import org.apache.beam.sdk.metrics.MetricName;
+import org.apache.beam.sdk.metrics.MetricQueryResults;
+import org.apache.beam.sdk.metrics.MetricResult;
+import org.apache.beam.sdk.metrics.MetricResults;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.annotations.VisibleForTesting;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toList;
+import static org.apache.beam.runners.core.metrics.MetricsContainerStepMap.asAttemptedOnlyMetricResults;
+
+/**
+ * An adapter between the {@link MetricsContainerStepMap} and Codahale's {@link Metric} interface.
+ */
+class SparkBeamMetric implements Metric {
+  private static final String ILLEGAL_CHARACTERS = "[^A-Za-z0-9-]";
+
+  Map<String, ?> renderAll() {
+    Map<String, Object> metrics = new HashMap<>();
+    MetricResults metricResults =
+        asAttemptedOnlyMetricResults(MetricsAccumulator.getInstance().value());
+    MetricQueryResults metricQueryResults = metricResults.allMetrics();
+    for (MetricResult<Long> metricResult : metricQueryResults.getCounters()) {
+      metrics.put(renderName(metricResult), metricResult.getAttempted());
+    }
+    for (MetricResult<DistributionResult> metricResult : metricQueryResults.getDistributions()) {
+      DistributionResult result = metricResult.getAttempted();
+      metrics.put(renderName(metricResult) + ".count", result.getCount());
+      metrics.put(renderName(metricResult) + ".sum", result.getSum());
+      metrics.put(renderName(metricResult) + ".min", result.getMin());
+      metrics.put(renderName(metricResult) + ".max", result.getMax());
+      metrics.put(renderName(metricResult) + ".mean", result.getMean());
+    }
+    for (MetricResult<GaugeResult> metricResult : metricQueryResults.getGauges()) {
+      metrics.put(renderName(metricResult), metricResult.getAttempted().getValue());
+    }
+    return metrics;
+  }
+
+  @VisibleForTesting
+  String renderName(MetricResult<?> metricResult) {
+    MetricKey key = metricResult.getKey();
+    MetricName name = key.metricName();
+    String step = key.stepName();
+
+    ArrayList<String> pieces = new ArrayList<>();
+
+    if (step != null) {
+      step = step.replaceAll(ILLEGAL_CHARACTERS, "_");
+      if (step.endsWith("_")) {
+        step = step.substring(0, step.length() - 1);
+      }
+      pieces.add(step);
+    }
+
+    pieces.addAll(
+        ImmutableList.of(name.getNamespace(), name.getName()).stream()
+            .map(str -> str.replaceAll(ILLEGAL_CHARACTERS, "_"))
+            .collect(toList()));
+
+    return String.join(".", pieces);
+  }
+}

samuelmoss572/dataflowworkshop

runners/spark/src/main/java/org/apache/beam/runners/spark/structuredstreaming/aggregators/AggregatorsAccumulator.java
@@ -1,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.aggregators;
+
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.annotations.VisibleForTesting;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.util.AccumulatorV2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * For resilience, {@link AccumulatorV2 Accumulators} are required to be wrapped in a Singleton.
+ *
+ * @see <a
+ *     href="https://spark.apache.org/docs/latest/api/java/org/apache/spark/util/AccumulatorV2.html">accumulatorsV2</a>
+ */
+public class AggregatorsAccumulator {
+  private static final Logger LOG = LoggerFactory.getLogger(AggregatorsAccumulator.class);
+
+  private static final String ACCUMULATOR_NAME = "Beam.Aggregators";
+
+  private static volatile NamedAggregatorsAccumulator instance = null;
+
+  /** Init aggregators accumulator if it has not been initiated. This method is idempotent. */
+  public static void init(JavaSparkContext jsc) {
+    if (instance == null) {
+      synchronized (AggregatorsAccumulator.class) {
+        if (instance == null) {
+          NamedAggregators namedAggregators = new NamedAggregators();
+          NamedAggregatorsAccumulator accumulator =
+              new NamedAggregatorsAccumulator(namedAggregators);
+          jsc.sc().register(accumulator, ACCUMULATOR_NAME);
+
+          instance = accumulator;
+        }
+      }
+      LOG.info("Instantiated aggregators accumulator: " + instance.value());
+    }
+  }
+
+  public static NamedAggregatorsAccumulator getInstance() {
+    if (instance == null) {
+      throw new IllegalStateException("Aggregrators accumulator has not been instantiated");
+    } else {
+      return instance;
+    }
+  }
+
+  @VisibleForTesting
+  public static void clear() {
+    synchronized (AggregatorsAccumulator.class) {
+      instance = null;
+    }
+  }
+}

runners/spark/src/main/java/org/apache/beam/runners/spark/structuredstreaming/aggregators/NamedAggregators.java
@@ -1,0 +1,112 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.aggregators;
+
+import org.apache.beam.sdk.transforms.Combine;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.ImmutableMap;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.Maps;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * This class wraps a map of named aggregators. Spark expects that all accumulators be declared
+ * before a job is launched. Beam allows aggregators to be used and incremented on the fly. We
+ * create a map of named aggregators and instantiate in the the spark context before the job is
+ * launched. We can then add aggregators on the fly in Spark.
+ */
+public class NamedAggregators implements Serializable {
+  /** Map from aggregator name to current state. */
+  private final Map<String, State<?, ?, ?>> mNamedAggregators = new TreeMap<>();
+
+  /** Constructs a new NamedAggregators instance. */
+  public NamedAggregators() {}
+
+  /**
+   * @param name Name of aggregator to retrieve.
+   * @param typeClass Type class to cast the value to.
+   * @param <T> Type to be returned.
+   * @return the value of the aggregator associated with the specified name, or <code>null</code> if
+   *     the specified aggregator could not be found.
+   */
+  public <T> T getValue(String name, Class<T> typeClass) {
+    final State<?, ?, ?> state = mNamedAggregators.get(name);
+    return state != null ? typeClass.cast(state.render()) : null;
+  }
+
+  /** @return a map of all the aggregator names and their <b>rendered </b>values */
+  public Map<String, ?> renderAll() {
+    return ImmutableMap.copyOf(Maps.transformValues(mNamedAggregators, State::render));
+  }
+
+  /**
+   * Merges another NamedAggregators instance with this instance.
+   *
+   * @param other The other instance of named aggregators ot merge.
+   * @return This instance of Named aggregators with associated states updated to reflect the other
+   *     instance's aggregators.
+   */
+  public NamedAggregators merge(
+      NamedAggregators other) {
+    for (Map.Entry<String, State<?, ?, ?>> e : other.mNamedAggregators.entrySet()) {
+      String key = e.getKey();
+      State<?, ?, ?> otherValue = e.getValue();
+      mNamedAggregators.merge(key, otherValue, NamedAggregators::merge);
+    }
+    return this;
+  }
+
+  /**
+   * Helper method to merge States whose generic types aren't provably the same, so require some
+   * casting.
+   */
+  @SuppressWarnings("unchecked")
+  private static <InputT, InterT, OutputT> State<InputT, InterT, OutputT> merge(
+      State<?, ?, ?> s1, State<?, ?, ?> s2) {
+    return ((State<InputT, InterT, OutputT>) s1).merge((State<InputT, InterT, OutputT>) s2);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    for (Map.Entry<String, State<?, ?, ?>> e : mNamedAggregators.entrySet()) {
+      sb.append(e.getKey()).append(": ").append(e.getValue().render()).append(" ");
+    }
+    return sb.toString();
+  }
+
+  /**
+   * @param <InputT> Input data type
+   * @param <InterT> Intermediate data type (useful for averages)
+   * @param <OutputT> Output data type
+   */
+  public interface State<InputT, InterT, OutputT> extends Serializable {
+
+    /** @param element new element to update state */
+    void update(InputT element);
+
+    State<InputT, InterT, OutputT> merge(State<InputT, InterT, OutputT> other);
+
+    InterT current();
+
+    OutputT render();
+
+    Combine.CombineFn<InputT, InterT, OutputT> getCombineFn();
+  }
+}

runners/spark/src/main/java/org/apache/beam/runners/spark/structuredstreaming/metrics/MetricsAccumulator.java
@@ -1,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.metrics;
+
+import org.apache.beam.runners.core.metrics.MetricsContainerStepMap;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.annotations.VisibleForTesting;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.util.AccumulatorV2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * For resilience, {@link AccumulatorV2 Accumulators} are required to be wrapped in a Singleton.
+ *
+ * @see <a
+ *     href="https://spark.apache.org/docs/latest/api/java/org/apache/spark/util/AccumulatorV2.html">accumulatorsV2</a>
+ */
+public class MetricsAccumulator {
+  private static final Logger LOG = LoggerFactory.getLogger(MetricsAccumulator.class);
+
+  private static final String ACCUMULATOR_NAME = "Beam.Metrics";
+
+  private static volatile MetricsContainerStepMapAccumulator instance = null;
+
+  /** Init metrics accumulator if it has not been initiated. This method is idempotent. */
+  public static void init(JavaSparkContext jsc) {
+    if (instance == null) {
+      synchronized (MetricsAccumulator.class) {
+        if (instance == null) {
+          MetricsContainerStepMap metricsContainerStepMap = new MetricsContainerStepMap();
+          MetricsContainerStepMapAccumulator accumulator =
+              new MetricsContainerStepMapAccumulator(metricsContainerStepMap);
+          jsc.sc().register(accumulator, ACCUMULATOR_NAME);
+
+          instance = accumulator;
+        }
+      }
+      LOG.info("Instantiated metrics accumulator: " + instance.value());
+    }
+  }
+
+  public static MetricsContainerStepMapAccumulator getInstance() {
+    if (instance == null) {
+      throw new IllegalStateException("Metrics accumulator has not been instantiated");
+    } else {
+      return instance;
+    }
+  }
+
+  @VisibleForTesting
+  public static void clear() {
+    synchronized (MetricsAccumulator.class) {
+      instance = null;
+    }
+  }
+}

runners/spark/src/main/java/org/apache/beam/runners/spark/structuredstreaming/metrics/SparkBeamMetric.java
@@ -1,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.metrics;
+
+import com.codahale.metrics.Metric;
+import org.apache.beam.runners.core.metrics.MetricsContainerStepMap;
+import org.apache.beam.sdk.metrics.DistributionResult;
+import org.apache.beam.sdk.metrics.GaugeResult;
+import org.apache.beam.sdk.metrics.MetricKey;
+import org.apache.beam.sdk.metrics.MetricName;
+import org.apache.beam.sdk.metrics.MetricQueryResults;
+import org.apache.beam.sdk.metrics.MetricResult;
+import org.apache.beam.sdk.metrics.MetricResults;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.annotations.VisibleForTesting;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toList;
+import static org.apache.beam.runners.core.metrics.MetricsContainerStepMap.asAttemptedOnlyMetricResults;
+
+/**
+ * An adapter between the {@link MetricsContainerStepMap} and Codahale's {@link Metric} interface.
+ */
+class SparkBeamMetric implements Metric {
+  private static final String ILLEGAL_CHARACTERS = "[^A-Za-z0-9-]";
+
+  Map<String, ?> renderAll() {
+    Map<String, Object> metrics = new HashMap<>();
+    MetricResults metricResults =
+        asAttemptedOnlyMetricResults(MetricsAccumulator.getInstance().value());
+    MetricQueryResults metricQueryResults = metricResults.allMetrics();
+    for (MetricResult<Long> metricResult : metricQueryResults.getCounters()) {
+      metrics.put(renderName(metricResult), metricResult.getAttempted());
+    }
+    for (MetricResult<DistributionResult> metricResult : metricQueryResults.getDistributions()) {
+      DistributionResult result = metricResult.getAttempted();
+      metrics.put(renderName(metricResult) + ".count", result.getCount());
+      metrics.put(renderName(metricResult) + ".sum", result.getSum());
+      metrics.put(renderName(metricResult) + ".min", result.getMin());
+      metrics.put(renderName(metricResult) + ".max", result.getMax());
+      metrics.put(renderName(metricResult) + ".mean", result.getMean());
+    }
+    for (MetricResult<GaugeResult> metricResult : metricQueryResults.getGauges()) {
+      metrics.put(renderName(metricResult), metricResult.getAttempted().getValue());
+    }
+    return metrics;
+  }
+
+  @VisibleForTesting
+  String renderName(MetricResult<?> metricResult) {
+    MetricKey key = metricResult.getKey();
+    MetricName name = key.metricName();
+    String step = key.stepName();
+
+    ArrayList<String> pieces = new ArrayList<>();
+
+    if (step != null) {
+      step = step.replaceAll(ILLEGAL_CHARACTERS, "_");
+      if (step.endsWith("_")) {
+        step = step.substring(0, step.length() - 1);
+      }
+      pieces.add(step);
+    }
+
+    pieces.addAll(
+        ImmutableList.of(name.getNamespace(), name.getName()).stream()
+            .map(str -> str.replaceAll(ILLEGAL_CHARACTERS, "_"))
+            .collect(toList()));
+
+    return String.join(".", pieces);
+  }
+}