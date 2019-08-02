spotify/beam

Added using CachedSideInputReader

runners/spark-structured-streaming/src/main/java/org/apache/beam/runners/spark/structuredstreaming/translation/utils/CachedSideInputReader.java
@@ -1,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.translation.utils;
+
+import org.apache.beam.runners.core.SideInputReader;
+import org.apache.beam.runners.spark.util.SideInputStorage.Key;
+import org.apache.beam.runners.spark.util.SideInputStorage.Value;
+import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
+import org.apache.beam.sdk.values.PCollectionView;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.cache.Cache;
+import org.apache.spark.util.SizeEstimator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+import java.util.concurrent.ExecutionException;
+
+/** {@link SideInputReader} that caches materialized views. */
+public class CachedSideInputReader implements SideInputReader {
+
+  private static final Logger LOG = LoggerFactory.getLogger(CachedSideInputReader.class);
+
+  /**
+   * Create a new cached {@link SideInputReader}.
+   *
+   * @param delegate wrapped reader
+   * @return cached reader
+   */
+  public static CachedSideInputReader of(SideInputReader delegate) {
+    return new CachedSideInputReader(delegate);
+  }
+
+  /** Wrapped {@link SideInputReader} which results will be cached. */
+  private final SideInputReader delegate;
+
+  private CachedSideInputReader(SideInputReader delegate) {
+    this.delegate = delegate;
+  }
+
+  @Nullable
+  @Override
+  public <T> T get(PCollectionView<T> view, BoundedWindow window) {
+    @SuppressWarnings("unchecked")
+    final Cache<Key<T>, Value<T>> materializedCasted =
+        (Cache) SideInputStorage.getMaterializedSideInputs();
+
+    Key<T> sideInputKey = new Key<>(view, window);
+
+    try {
+      Value<T> cachedResult =
+          materializedCasted.get(
+              sideInputKey,
+              () -> {
+                final T result = delegate.get(view, window);
+                LOG.debug(
+                    "Caching de-serialized side input for {} of size [{}B] in memory.",
+                    sideInputKey,
+                    SizeEstimator.estimate(result));
+
+                return new Value<>(result);
+              });
+      return cachedResult.getValue();
+    } catch (ExecutionException e) {
+      throw new RuntimeException(e.getCause());
+    }
+  }
+
+  @Override
+  public <T> boolean contains(PCollectionView<T> view) {
+    return delegate.contains(view);
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return delegate.isEmpty();
+  }
+}

runners/spark-structured-streaming/src/main/java/org/apache/beam/runners/spark/structuredstreaming/translation/utils/SideInputStorage.java
@@ -1,0 +1,104 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.translation.utils;
+
+import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
+import org.apache.beam.sdk.values.PCollectionView;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.cache.Cache;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.cache.CacheBuilder;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Cache deserialized side inputs for executor so every task doesn't need to deserialize them again.
+ * Side inputs are stored in {@link Cache} with 5 minutes expireAfterAccess.
+ */
+class SideInputStorage {
+
+  /** JVM deserialized side input cache. */
+  private static final Cache<Key<?>, Value<?>> materializedSideInputs =
+      CacheBuilder.newBuilder().expireAfterAccess(5, TimeUnit.MINUTES).build();
+
+  static Cache<Key<?>, Value<?>> getMaterializedSideInputs() {
+    return materializedSideInputs;
+  }
+
+  /**
+   * Composite key of {@link PCollectionView} and {@link BoundedWindow} used to identify
+   * materialized results.
+   *
+   * @param <T> type of result
+   */
+  public static class Key<T> {
+
+    private final PCollectionView<T> view;
+    private final BoundedWindow window;
+
+    Key(PCollectionView<T> view, BoundedWindow window) {
+      this.view = view;
+      this.window = window;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      Key<?> key = (Key<?>) o;
+      return Objects.equals(view, key.view) && Objects.equals(window, key.window);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(view, window);
+    }
+
+    @Override
+    public String toString() {
+      String pName = view.getPCollection() != null ? view.getPCollection().getName() : "Unknown";
+      return "Key{"
+          + "view="
+          + view.getTagInternal()
+          + " of PCollection["
+          + pName
+          + "], window="
+          + window
+          + '}';
+    }
+  }
+
+  /**
+   * Null value is not allowed in guava's Cache and is valid in SideInput so we use wrapper for
+   * cache value.
+   */
+  public static class Value<T> {
+    final T value;
+
+    Value(T value) {
+      this.value = value;
+    }
+
+    public T getValue() {
+      return value;
+    }
+  }
+}

samuelmoss572/dataflowworkshop

runners/spark-structured-streaming/src/main/java/org/apache/beam/runners/spark/structuredstreaming/translation/utils/CachedSideInputReader.java
@@ -1,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.translation.utils;
+
+import org.apache.beam.runners.core.SideInputReader;
+import org.apache.beam.runners.spark.util.SideInputStorage.Key;
+import org.apache.beam.runners.spark.util.SideInputStorage.Value;
+import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
+import org.apache.beam.sdk.values.PCollectionView;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.cache.Cache;
+import org.apache.spark.util.SizeEstimator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+import java.util.concurrent.ExecutionException;
+
+/** {@link SideInputReader} that caches materialized views. */
+public class CachedSideInputReader implements SideInputReader {
+
+  private static final Logger LOG = LoggerFactory.getLogger(CachedSideInputReader.class);
+
+  /**
+   * Create a new cached {@link SideInputReader}.
+   *
+   * @param delegate wrapped reader
+   * @return cached reader
+   */
+  public static CachedSideInputReader of(SideInputReader delegate) {
+    return new CachedSideInputReader(delegate);
+  }
+
+  /** Wrapped {@link SideInputReader} which results will be cached. */
+  private final SideInputReader delegate;
+
+  private CachedSideInputReader(SideInputReader delegate) {
+    this.delegate = delegate;
+  }
+
+  @Nullable
+  @Override
+  public <T> T get(PCollectionView<T> view, BoundedWindow window) {
+    @SuppressWarnings("unchecked")
+    final Cache<Key<T>, Value<T>> materializedCasted =
+        (Cache) SideInputStorage.getMaterializedSideInputs();
+
+    Key<T> sideInputKey = new Key<>(view, window);
+
+    try {
+      Value<T> cachedResult =
+          materializedCasted.get(
+              sideInputKey,
+              () -> {
+                final T result = delegate.get(view, window);
+                LOG.debug(
+                    "Caching de-serialized side input for {} of size [{}B] in memory.",
+                    sideInputKey,
+                    SizeEstimator.estimate(result));
+
+                return new Value<>(result);
+              });
+      return cachedResult.getValue();
+    } catch (ExecutionException e) {
+      throw new RuntimeException(e.getCause());
+    }
+  }
+
+  @Override
+  public <T> boolean contains(PCollectionView<T> view) {
+    return delegate.contains(view);
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return delegate.isEmpty();
+  }
+}

runners/spark-structured-streaming/src/main/java/org/apache/beam/runners/spark/structuredstreaming/translation/utils/SideInputStorage.java
@@ -1,0 +1,104 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.spark.structuredstreaming.translation.utils;
+
+import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
+import org.apache.beam.sdk.values.PCollectionView;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.cache.Cache;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.cache.CacheBuilder;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Cache deserialized side inputs for executor so every task doesn't need to deserialize them again.
+ * Side inputs are stored in {@link Cache} with 5 minutes expireAfterAccess.
+ */
+class SideInputStorage {
+
+  /** JVM deserialized side input cache. */
+  private static final Cache<Key<?>, Value<?>> materializedSideInputs =
+      CacheBuilder.newBuilder().expireAfterAccess(5, TimeUnit.MINUTES).build();
+
+  static Cache<Key<?>, Value<?>> getMaterializedSideInputs() {
+    return materializedSideInputs;
+  }
+
+  /**
+   * Composite key of {@link PCollectionView} and {@link BoundedWindow} used to identify
+   * materialized results.
+   *
+   * @param <T> type of result
+   */
+  public static class Key<T> {
+
+    private final PCollectionView<T> view;
+    private final BoundedWindow window;
+
+    Key(PCollectionView<T> view, BoundedWindow window) {
+      this.view = view;
+      this.window = window;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      Key<?> key = (Key<?>) o;
+      return Objects.equals(view, key.view) && Objects.equals(window, key.window);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(view, window);
+    }
+
+    @Override
+    public String toString() {
+      String pName = view.getPCollection() != null ? view.getPCollection().getName() : "Unknown";
+      return "Key{"
+          + "view="
+          + view.getTagInternal()
+          + " of PCollection["
+          + pName
+          + "], window="
+          + window
+          + '}';
+    }
+  }
+
+  /**
+   * Null value is not allowed in guava's Cache and is valid in SideInput so we use wrapper for
+   * cache value.
+   */
+  public static class Value<T> {
+    final T value;
+
+    Value(T value) {
+      this.value = value;
+    }
+
+    public T getValue() {
+      return value;
+    }
+  }
+}